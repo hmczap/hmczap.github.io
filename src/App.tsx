c7a0c9c87ec7dd509a61862465ca381e82ed042d
hmczap <114511170+hmczap@users.noreply.github.com>
2024-10-20 19:06:14 -0400

    Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+//   const [count, setCount] = useState(0)
 
   return (
     <div className="container">
